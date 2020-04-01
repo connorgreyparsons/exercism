d1d61de22628010cf610cb6a873b623a23d80e7d
cpars <cpars@cpars-pc>
2020-04-01 14:34:49 -0500

    refactor ifs to and/or

--- scheme/leap/leap.scm
+++ scheme/leap/leap.scm
@@ -1,6 +1,6 @@
 (import (rnrs))
 
 (define (leap-year? year)
-  (if (= (remainder year 400) 0) #t
-      (if (and (not (= (remainder year 100) 0))
-              (= (remainder year 4) 0)) #t #f)))
+  (or (= (remainder year 400) 0)
+      (and (not (= (remainder year 100) 0))
+           (= (remainder year 4) 0))))
