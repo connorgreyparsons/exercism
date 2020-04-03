15d8174ed19c187d66023b01352d8c2f7f4e8d4d
cpars <cpars@cpars-pc>
2020-04-03 09:37:35 -0500

    refactor recursion to map, better equality checks

--- scheme/rna-transcription/rna-transcription.scm
+++ scheme/rna-transcription/rna-transcription.scm
@@ -1,10 +1,11 @@
 (import (rnrs))
 
 (define (dna->rna dna)
-  (let loop((ls (string->list dna)))
-    (cond
-     ((null? ls) "")
-     ((eq? (car ls) #\G) (string-append "C" (loop (cdr ls))))
-     ((eq? (car ls) #\C) (string-append "G" (loop (cdr ls))))
-     ((eq? (car ls) #\T) (string-append "A" (loop (cdr ls))))
-     ((eq? (car ls) #\A) (string-append "U" (loop (cdr ls)))))))
+  (list->string
+   (map (lambda (c)
+          (cond
+           ((eqv? c #\G) #\C)
+           ((eqv? c #\C) #\G)
+           ((eqv? c #\T) #\A)
+           ((eqv? c #\A) #\U)))
+        (string->list dna))))
